d107856e8412ab9dd22e81c6e7178d7d0b6572b2
Sandeep <itissandeep98@gmail.com>
2020-06-20 05:15:54 +0530

    url fixed for gh pages

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Main />
         </div>
